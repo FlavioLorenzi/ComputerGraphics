fbb766f1020635974022a6aa25071e6dac1fdddb
Flavio Lorenzi <43888007+FlavioLorenzi@users.noreply.github.com>
2020-12-16 09:43:55 +0100

    Add files via upload

---
+++ VolumetricPath/yocto_pathtrace/CMakeFiles/yocto_pathtrace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/libyocto_pathtrace.a"
+  "../../bin/libyocto_pathtrace.pdb"
+  "CMakeFiles/yocto_pathtrace.dir/yocto_pathtrace.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yocto_pathtrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
